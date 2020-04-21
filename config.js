6a04b32c38b3b134db5d98497c35bb29e83193cc
Brett Bender <brett@brettb.xyz>
2020-04-21 02:26:08 -0500

    Updates

--- config.js
+++ config.js
@@ -4,6 +4,12 @@ module.exports = {
   "baseUrl": "https://status.brettb.xyz",
   "description": "The Brett Bender status page",
   "defaultLocale": "en",
+  "head": {
+    "script": [
+      { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
+      { src: "/js/netlifyRedirect.js" }
+    ]
+  },
   "locales": [
     {
       "code": "en",
